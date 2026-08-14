6ce006c18795543ce7ad26200af19173ad49071d
Amer Thander <amerthander@gmail.com>
2026-08-14 20:45:47 +0530

    Added Questions in JS

---
+++ script.js
@@ -0,0 +1,66 @@
+
+const startScreen = document.getElementById('start-screen');
+const quizScreen = document.getElementById("quiz-screen");
+const resultScreen = document.getElementById("result-screen");
+const startButton = document.getElementById("start-btn");
+const questionText = document.getElementById("question-text");
+const answerContainer = document.getElementById("answer-container");
+const currentQuestionSpan = document.getElementById("current-question");
+const totalQuestionsSpan = document.getElementById("total-questions");
+const scoreSpan = document.getElementById("score");
+const finalScoreSpan = document.getElementById("final-score");
+const maxScoreSpan = document.getElementById("max-score");
+const resultMessage = document.getElementById("result-message");
+const restartButton = document.getElementById("restart-btn");
+const progressBar = document.getElementById("progress-bar");
+
+
+const quizQuestions = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false },
+            { text: "Paris", correct: true },   
+            { text: "London", correct: false },
+        ],
+    },
+     {
+        question: "Which planet is known as the Red Planet?",
+        answers: [
+            { text: "Venus", correct: false },
+            { text: "Jupiter", correct: false },
+            { text: "Saturn", correct: false },
+            { text: "Mars", correct: true },
+        ],
+    },
+     {
+        question: "What is the largest ocean on Earth?",
+        answers: [
+            { text: "Pacific Ocean", correct: true },
+            { text: "Atlantic Ocean", correct: false },
+            { text: "Indian Ocean", correct: false },
+            { text: "Pacific Ocean", correct: false },
+        ],
+    },
+     {
+        question: "Which of this is NOT a coding language?",
+        answers: [
+            { text: "Java", correct: false },
+            { text: "Python", correct: false },
+            { text: "Banana", correct: true },  
+            { text: "JavaScript", correct: false },
+        ],
+    },
+     {
+        question: "What is the chemical symbol for gold?",
+        answers: [
+            { text: "Go", correct: false },
+            { text: "Gd", correct: false },
+            { text: "Au", correct: true },  
+            { text: "Ag", correct: false },
+        ],
+    },,
+];
+
+
